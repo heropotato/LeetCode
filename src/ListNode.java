8c435878f2efaae2059308b0a4fc8464dda1d483
Yong WEN <heropotato1130@msn.com>
2013-09-12 12:09:00 -0400

    Update

--- src/ListNode.java
+++ src/ListNode.java
@@ -14,4 +14,5 @@ public class ListNode {
         val = x;
         next = null;
     }
+
 }
---
+++ src/TreeNode.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 6/24/13
+ * Time: 11:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+    }
+
+}
