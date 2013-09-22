3cc75d063504fdedcec4602a6d952c9b5096211a
Yong WEN <heropotato1130@msn.com>
2013-09-22 11:43:08 -0400

    Palindrome Number

--- src/isPalindrome.java
+++ src/isPalindrome.java
@@ -17,6 +17,7 @@ public class isPalindrome {
     public boolean isPalindrome(int x) {
         // Start typing your Java solution below
         // DO NOT write main() function
+
         if (x < 0) return false;
 
         int a = x, b = 0;
