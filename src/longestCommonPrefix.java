51850a5d7853ef2b35ba5946828c8539f560db75
Yong WEN <heropotato1130@msn.com>
2013-09-23 10:07:02 -0400

    Longest Common Prefix

--- src/longestCommonPrefix.java
+++ src/longestCommonPrefix.java
@@ -19,9 +19,24 @@ public class longestCommonPrefix {
         // Start typing your Java solution below
         // DO NOT write main() function
 
+        if(strs.length == 0) return "";
+        String res = strs[0];
 
+        for(String s:strs){
 
+            if(s.isEmpty()) return "";
+            int tempLength = Math.min(res.length(), s.length());
+            int pointer = 0;
 
+            while(pointer < tempLength){
+                if(res.charAt(pointer) != s.charAt(pointer)) break;
+                pointer++;
+            }
+
+            res = res.substring(0, pointer);
+
+        }
+
+        return res;
     }
-
 }
