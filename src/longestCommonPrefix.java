9eaa3fdf4a718d90a602f797a8bee3f2a48aab36
Yong WEN <heropotato1130@msn.com>
2013-09-23 09:12:28 -0400

    Longest Common Prefix

---
+++ src/longestCommonPrefix.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 9/22/13
+ * Time: 10:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class longestCommonPrefix {
+
+    /*
+    *
+    * Longest Common Prefix
+    *
+    * Write a function to find the longest common prefix string amongst an array of strings.
+    *
+    * */
+
+    public String longestCommonPrefix(String[] strs) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+
+
+
+
+    }
+
+}
