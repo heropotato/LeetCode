983ab0251a04e81fed462bdcf035aeef44eea90f
Yong WEN <heropotato1130@msn.com>
2013-09-24 11:42:34 -0400

    4Sum

---
+++ src/fourSum.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 9/24/13
+ * Time: 10:59 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class fourSum {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (num.length < 4) return res;
+
+        Arrays.sort(num);
+        HashSet<ArrayList<Integer>> hashSet = new HashSet<ArrayList<Integer>>();
+        int pointer = 0;
+
+        while (pointer <= num.length - 4) {
+            int tempTarget = target - num[pointer];
+            int[] tempNum = Arrays.copyOfRange(num, pointer + 1, num.length);
+
+            for (ArrayList<Integer> list : threeSum(tempNum, tempTarget, num[pointer])) {
+                if (hashSet.add(list)) res.add(list);
+            }
+            pointer++;
+        }
+
+        return res;
+    }
+
+    private ArrayList<ArrayList<Integer>> threeSum(int[] num, int target, int pre) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (num.length < 3) return res;
+
+        for (int i = 0; i < num.length - 2; i++) {
+            int lPointer = i + 1;
+            int rPointer = num.length - 1;
+
+            while (lPointer < rPointer) {
+                if (num[i] + num[lPointer] + num[rPointer] > target) {
+                    rPointer--;
+                } else if (num[i] + num[lPointer] + num[rPointer] < target) {
+                    lPointer++;
+                } else {
+                    ArrayList<Integer> temp = new ArrayList<Integer>();
+                    temp.add(pre);
+                    temp.add(num[i]);
+                    temp.add(num[lPointer]);
+                    temp.add(num[rPointer]);
+
+                    res.add(temp);
+                    rPointer--;
+                    lPointer++;
+                }
+            }
+
+        }
+
+        return res;
+    }
+}
