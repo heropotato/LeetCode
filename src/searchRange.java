570925da4b9563ad432c1500313a4ed9ae0e4441
Yong WEN <heropotato1130@msn.com>
2013-09-28 22:10:58 -0400

    Search for a Range

--- src/searchRange.java
+++ src/searchRange.java
@@ -11,12 +11,13 @@ import java.util.HashSet;
 public class searchRange {
     /*
     *
+    * Search for a Range
     *
+    * Given a sorted array of integers, find the starting and ending position of a given target value.
     *
+    * Your algorithm's runtime complexity must be in the order of O(log n).
     *
-    *
-    *
-    *
+    * If the target is not found in the array, return [-1, -1].
     *
     * */
 
