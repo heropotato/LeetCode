2277d0369944f9e99ec05460c6be9857c6bb23b8
Yong WEN <heropotato1130@msn.com>
2013-10-05 12:23:33 -0400

    Pow(x, n)

--- src/pow.java
+++ src/pow.java
@@ -20,11 +20,18 @@ public class pow {
         // Start typing your Java solution below
         // DO NOT write main() function
 
-        if (n == 0) return 1.0;
-        double half = pow(x, n / 2);
-        if (n % 2 == 0) return half * half;
-        else if (n > 0) return half * half * x;
-        else return half * half / x;
+        if(n==0){return 1.0;}
+        if(n==1){return x;}
+        if(n==-1){return 1/x;}
+
+        if(n%2==0){
+            double temp = pow(x, n/2);
+            return temp*temp;
+        }
+        else{
+            double temp = pow(x, (n-1)/2);
+            return x*temp*temp;
+        }
     }
 
 }
