24c0af4d1fde58469707faab22c740931a3e40fc
Yong WEN <heropotato1130@msn.com>
2013-10-08 13:42:22 -0400

    Intervals

---
+++ src/Interval.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 10/8/13
+ * Time: 1:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Interval {
+
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+
+}
---
+++ src/merge.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 10/8/13
+ * Time: 1:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class merge {
+
+    /*
+    *
+    * Merge Intervals
+    *
+    * Given a collection of intervals, merge all overlapping intervals.
+    *
+    * For example,
+    * Given [1,3],[2,6],[8,10],[15,18],
+    * return [1,6],[8,10],[15,18].
+    *
+    *
+    * */
+
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+
+    }
+
+ }
