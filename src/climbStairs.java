fcb61dbc25cf1c6dec195aa3244aa7da8b74881d
Yong WEN <heropotato1130@msn.com>
2013-10-11 17:31:28 -0400

    Climbing Stairs

---
+++ src/climbStairs.java
@@ -0,0 +1,32 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 10/11/13
+ * Time: 5:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class climbStairs {
+
+    /*
+    *
+    * Climbing Stairs
+    *
+    * You are climbing a stair case. It takes n steps to reach to the top
+    *
+    * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    *
+    *
+    * */
+
+    public int climbStairs(int n) {
+        if (n < 2) return n;
+        int[] count = new int[n];
+        count[0] = 1;
+        count[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            count[i] = count[i - 1] + count[i - 2];
+        }
+        return count[n - 1];
+    }
+}
