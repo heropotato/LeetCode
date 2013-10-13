bd273adc4d3d116da0f6428e4d5bc10709fccc6a
Yong WEN <heropotato1130@msn.com>
2013-10-12 23:45:18 -0400

    Permutations

--- src/permute.java
+++ src/permute.java
@@ -20,6 +20,7 @@ public class permute {
     * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
     *
     *
+    *
     * */
 
     public ArrayList<ArrayList<Integer>> permute(int[] num) {
