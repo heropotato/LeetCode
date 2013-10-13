4e1c05da2b751978160445943fb16cabd1b1e2fb
Yong WEN <heropotato1130@msn.com>
2013-10-13 13:49:22 -0400

    Combinations

--- src/combine.java
+++ src/combine.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -29,10 +32,98 @@ public class combine {
     *
     * */
 
-    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
+    public static void main(String[] args) {
+/*
+        HashSet<ArrayList<Integer>> hashSet = new HashSet<ArrayList<Integer>>();
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        b.add(2);
+        b.add(1);
+        hashSet.add(new ArrayList<Integer>(a));
+
+        System.out.println("HEY: " + hashSet.add(new ArrayList<Integer>(b)));*/
+
+        ArrayList<Integer> t = new ArrayList<Integer>(8);
+        long start = System.currentTimeMillis();
+        System.out.println("HEY: " + combine(13, 10));
+        long end = System.currentTimeMillis();
+        System.out.println("Time: " + (end - start));
 
     }
 
+    public static ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+
+        //get inspired form yuanx's GitHub repository: https://github.com/yuanx/leetcode/blob/master/Combinations.java
+        //a truly elegant back trace recursion solution
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        generateList(n, k, new ArrayList<Integer>(), res, 0);
+        return res;
+    }
+
+    private static void generateList(int n, int k, ArrayList<Integer> currentCombination, ArrayList<ArrayList<Integer>> res, int start) {
+
+        //base case: once currentCombination size is matching k, then it is considered as a valid combination, add to result
+        if (currentCombination.size() == k) {
+            ArrayList<Integer> temp = new ArrayList<Integer>(currentCombination);
+            res.add(temp);
+            return;
+        }
+
+        //back trace
+        //add each number from 1 to n to a combination list in ascending order
+        for (int i = start; i < n; i++) {
+            //add each number from 1 to n to a combination list in ascending order
+            currentCombination.add(i + 1);
+            //recursively add greater number to current combination list one by one
+            //this is truly elegant:
+            // 1, it covered every single possibilities;
+            // 2, due to the ascending order, each adding cannot create duplicate combinations
+            //e.g., (n = 4, k = 2), the first combination is evolving as below:
+            //[1] --> [1, 2], [1, 3], [1, 4] --> each length is 2 as well as k, all will be added to result
+            generateList(n, k, currentCombination, res, i + 1);
+            //if the move is failed (without adding to the result list), it goes back here
+            //of course, remove the failed adding.
+            currentCombination.remove(currentCombination.size() - 1);
+        }
+
+    }
+
+/*
+    public static ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (k == 0) return null;
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> original = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            temp.add(i);
+            res.add(temp);
+            original.add(i);
+        }
+
+        HashSet<ArrayList<Integer>> hashSet = new HashSet<ArrayList<Integer>>();
+
+        if(k>1){
+            res = new ArrayList<ArrayList<Integer>>();
+            for (Integer integer: original){
+                for (ArrayList<Integer> arrayList: combine(n, k-1)){
+                    ArrayList<Integer> tempList = new ArrayList<Integer>(arrayList);
+                    if (tempList.contains(integer)) continue;
+                    tempList.add(integer);
+                    Collections.sort(tempList);
+                    if(hashSet.add(tempList)) res.add(tempList);
+                }
+            }
+        }
+
+        return res;
+    }
+     */
 }
