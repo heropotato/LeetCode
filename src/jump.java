1c0453bd62c9baf257bf104fa5cb5f90d531a2de
Yong WEN <heropotato1130@msn.com>
2013-10-12 23:40:30 -0400

    Jump Game II

--- src/jump.java
+++ src/jump.java
@@ -24,6 +24,7 @@ public class jump {
 * The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
 *
 *
+*
 * */
 
     public int jump(int[] A) {
