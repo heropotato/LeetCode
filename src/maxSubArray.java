75b4b6f61cb88bfdfe55780526f3b4cf914a37cc
Yong WEN <heropotato1130@msn.com>
2013-10-12 23:43:33 -0400

    Maximum Subarray

--- src/maxSubArray.java
+++ src/maxSubArray.java
@@ -18,6 +18,7 @@ public class maxSubArray {
     * For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
     * the contiguous subarray [4,−1,2,1] has the largest sum = 6.
     *
+    *
     * */
 
     public int maxSubArray(int[] A) {
