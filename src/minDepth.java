d30d58de32b3e05fc8627b976f758a7f70b74cf1
Yong WEN <heropotato1130@msn.com>
2013-10-25 14:41:43 -0400

    Minimum Depth of Binary Tree

--- src/minDepth.java
+++ src/minDepth.java
@@ -18,17 +18,15 @@ public class minDepth {
     *
     * */
 
+
     public int minDepth(TreeNode root) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if (root == null) return 0;
-
         if (root.left == null && root.right == null) return 1;
-
-        if (root.left == null) {
-            return minDepth(root.right)+1;
-        } else if (root.right == null) {
-            return minDepth(root.left)+1;
-        } else
-            return Math.min(minDepth(root.left), minDepth(root.right))+1;
+        else {
+            int leftDepth = root.left != null ? minDepth(root.left) : Integer.MAX_VALUE;
+            int rightDepth = root.right != null ? minDepth(root.right) : Integer.MAX_VALUE;
+            return Math.min(leftDepth, rightDepth) + 1;
+        }
     }
 }
