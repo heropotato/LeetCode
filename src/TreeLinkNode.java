65df8f62810fcde7cc2f1bf80898816f11438667
Yong WEN <heropotato1130@msn.com>
2013-10-25 17:09:38 -0400

    TreeLinkNode

---
+++ src/TreeLinkNode.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 10/25/13
+ * Time: 5:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TreeLinkNode {
+
+    //Definition for binary tree with next pointer.
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
---
+++ src/connect.java
@@ -0,0 +1,52 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 10/25/13
+ * Time: 5:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class connect {
+
+    /*
+    *
+    * Populating Next Right Pointers in Each Node
+    *
+    * Given a binary tree
+    *
+    *
+    * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+    *
+    * Initially, all next pointers are set to NULL.
+    *
+    * Note:
+    *
+    *
+    * You may only use constant extra space.
+    * You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+    * For example,
+    *
+    * Given the following perfect binary tree,
+    *
+    *       1
+    *     /  \
+    *    2    3
+    *   / \  / \
+    *  4  5  6  7
+    *
+    * After calling your function, the tree should look like:
+    *
+    *       1 -> NULL
+    *     /  \
+    *    2 -> 3 -> NULL
+    *   / \  / \
+    *  4->5->6->7 -> NULL
+    *
+    *
+    * */
+
+    public void connect(TreeLinkNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+
+    }
+
+ }
