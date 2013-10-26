08313970c8fc4fd0ca715b8e3940a5f3ef6cb500
Yong WEN <heropotato1130@msn.com>
2013-10-26 16:55:50 -0400

    Word Ladder II

--- src/findLadders.java
+++ src/findLadders.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created with IntelliJ IDEA.
@@ -39,39 +38,87 @@ public class findLadders {
     *
     * */
 
-     public int ladderLength(String start, String end, HashSet<String> dict) {
-        if (start.length() != end.length()) return 0;
-        if (start.equals("")) return 1;
-        if (dict.size() == 0) return 0;
-        int count = 1;
-        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<String>();
-        queue.add(start);
-        queue.add(null);
+    public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
 
+        // get inspired from http://discuss.leetcode.com/questions/1051/word-ladder-ii/2468
+
+
+        // method would be similar, except that, we need to store the path
+        // in addition, if we get the result, we didn't stop until we finish
+        // all the path of the same length
+        // visited map the string to the list of its ancestor
+        HashMap<String, HashSet<String>> visited = new HashMap<String, HashSet<String>>();
+        HashMap<String, Integer> level = new HashMap<String, Integer>();
+        LinkedList<String> queue = new LinkedList<String>();
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        if (start == null || end == null || start.length() != end.length()) {
+            return result;
+        }
+        // we also need to store the path from the start
+        HashSet<String> path = new HashSet<String>();
+        // we record the minimal length we get
+        int min_length = Integer.MAX_VALUE;
+        visited.put(start, path);
+        level.put(start, 1);
+        queue.add(start);
         while (!queue.isEmpty()) {
-            String str = queue.poll();
-            if (str != null) {
-                for (int i = 0; i < str.length(); i++) {
-                    for (char j = 'a'; j <= 'z'; j++) {
-                        if (j == str.charAt(i)) continue;
-                        char[] temp = str.toCharArray();
-                        temp[i] = j;
-                        String newString = new String(temp);
-                        if (newString.equals(end)) return count+1;
-                        if (dict.contains(newString)) {
-                            queue.add(newString);
-                            dict.remove(newString);
+            String s = queue.remove();
+            char[] chars = s.toCharArray();
+            for (int i = 0; i < s.length(); i++) {
+                char old = chars[i];
+                for (char c = 'a'; c <= 'z'; c++) {
+                    chars[i] = c;
+                    String s2 = new String(chars);
+                    // avoid circle
+                    // check whether it is in the dictionary
+                    // we only add the string which is nearer to the start
+                    if (dict.contains(s2) && (!level.containsKey(s2) || (level.containsKey(s2) && level.get(s2) > level.get(s)))) {
+                        // we update the ancestor of the string
+                        if (visited.containsKey(s2)) {
+                            visited.get(s2).add(s);
+                        } else {
+                            // we haven't seen this node before
+                            // thus we add it to the queue and also its ancestor
+                            path = new HashSet<String>();
+                            path.add(s);
+                            visited.put(s2, path);
+                            level.put(s2, level.get(s) + 1);
+                            queue.add(s2);
+                        }
+                    }
+                    if (s2.equals(end)) {
+                        // we found it
+                        // we will use back trace to found its path to start
+                        if (level.get(s) < min_length) {
+                            // it is shortest path
+                            ArrayList<String> entry = new ArrayList<String>();
+                            entry.add(end);
+                            result.addAll(back_trace(s, visited, entry));
+                            min_length = level.get(s) + 1;
+                        } else {
+                            // ok, all the remaining path should be longer
+                            break;
                         }
                     }
                 }
-            } else {
-                if (!queue.isEmpty()) {
-                    queue.add(null);
-                    count++;
-                }
+                chars[i] = old;
             }
         }
+        return result;
+    }
 
-        return 0;
+    private ArrayList<ArrayList<String>> back_trace(String end, HashMap<String, HashSet<String>> visited, ArrayList<String> path) {
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        ArrayList<String> entry = new ArrayList<String>(path);
+        entry.add(0, end);
+        if (visited.get(end).size() < 1) {
+            result.add(entry);
+            return result;
+        }
+        for (String str : visited.get(end)) {
+            result.addAll(back_trace(str, visited, entry));
+        }
+        return result;
     }
 }
