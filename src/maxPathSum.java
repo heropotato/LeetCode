3c96595cea7c079de2d343859ef240d6dcd539ba
Yong WEN <heropotato1130@msn.com>
2013-10-26 13:39:47 -0400

    Binary Tree Maximum Path Sum

--- src/maxPathSum.java
+++ src/maxPathSum.java
@@ -49,40 +49,4 @@ public class maxPathSum {
         maxSums.push(Math.max(maxSums.peek(), root.val + leftMax + rightMax));
         return Math.max(root.val + leftMax, root.val + rightMax);
     }
-
-/*
-    One needs to handle three cases:
-
-    Left tree path plus the current root.
-    Right tree path plus the current root.
-    The path passes through the root and hence one needs to consider the left path + current root + right path.
-    In addition compare with the max path so far and update accordingly.
-
- */
-
-
-/*
-
-    int maxPathSum(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int csum;
-        int maxsum = INT_MIN;
-        maxPathSumHelper(root, csum, maxsum);
-        return maxsum;
-
-    }
-    void maxPathSumHelper(TreeNode *node, int &csum, int &maxsum) {
-        if (!node) {
-            csum = 0;
-            return;
-        }
-        int lsum = 0, rsum = 0;
-        maxPathSumHelper(node->left, lsum, maxsum);
-        maxPathSumHelper(node->right, rsum, maxsum);
-        csum = max(node->val, max(node->val + lsum, node->val + rsum));
-        maxsum = max(maxsum, max(csum, node->val + lsum + rsum));
-    }
-
-    */
 }
