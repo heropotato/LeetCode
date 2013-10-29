1dd1fa752d1ae83227ea07b6b0ad93e15e3e276e
Yong WEN <heropotato1130@msn.com>
2013-10-28 23:31:56 -0400

    RandomListNode

---
+++ src/RandomListNode.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yongwen
+ * Date: 10/28/13
+ * Time: 11:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RandomListNode {
+    //Definition for singly-linked list with a random pointer.
+
+    int label;
+    RandomListNode next, random;
+
+    RandomListNode(int x) {
+        this.label = x;
+    }
+
+}
