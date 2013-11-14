2a89165941d72db11f4b1781c2364271354b691c
Yong WEN <heropotato1130@msn.com>
2013-11-13 22:36:36 -0500

    Insertion Sort List

--- src/insertionSortList.java
+++ src/insertionSortList.java
@@ -11,18 +11,6 @@ public class insertionSortList {
     *
     * Sort a linked list using insertion sort.
     *
-    *
-    * Definition for singly-linked list.
-    * public class ListNode {
-    *     int val;
-    *     ListNode next;
-    *     ListNode(int x) {
-    *         val = x;
-    *         next = null;
-    *     }
-    * }
-    *
-    *
     * */
 
     public ListNode insertionSortList(ListNode head) {
