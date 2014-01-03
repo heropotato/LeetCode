a22d01cb6d877ae73b52f1c50e97a5df0ab9f13d
Yong WEN <heropotato1130@msn.com>
2014-01-02 23:44:58 -0500

    Longest Valid Parentheses

--- src/longestValidParentheses.java
+++ src/longestValidParentheses.java
@@ -14,22 +14,17 @@ public class longestValidParentheses {
     * Longest Valid Parentheses
     *
     * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
-    *
     * For "(()", the longest valid parentheses substring is "()", which has length = 2.
-    *
     * Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
     *
-    *
-    *
     * */
-
 
     public int longestValidParentheses(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
 
         Stack<Integer> validTrack = new Stack<Integer>();
-        int res = 0, endOfValid = -1;
+        int res = 0, endOfInvalid = -1;
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
@@ -37,9 +32,9 @@ public class longestValidParentheses {
             } else {
                 if (!validTrack.isEmpty()) {
                     validTrack.pop();
-                    res = validTrack.isEmpty() ? Math.max(res, i - endOfValid) : Math.max(res, i - validTrack.peek());
+                    res = validTrack.isEmpty() ? Math.max(res, i - endOfInvalid) : Math.max(res, i - validTrack.peek());
                 } else {
-                    endOfValid = i;
+                    endOfInvalid = i;
                 }
             }
         }
