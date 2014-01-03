3385f1c40110a267353def1f77ba2be2fd731570
Yong WEN <heropotato1130@msn.com>
2014-01-02 21:57:55 -0500

    Next Permutation

--- src/nextPermutation.java
+++ src/nextPermutation.java
@@ -14,79 +14,55 @@ public class nextPermutation {
     * Next Permutation
     *
     * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
-    *
     * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
-    *
     * The replacement must be in-place, do not allocate extra memory.
     *
+    * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
     *
+    * 1,2,3 → 1,3,2
+    * 3,2,1 → 1,2,3
+    * 1,1,5 → 1,5,1
     *
     * */
-
 
     public void nextPermutation(int[] num) {
         // Start typing your Java solution below
         // DO NOT write main() function
 
-        int length = num.length;
-        if (length < 2) return;
+        // What is 'Next Permutation':
+        // Step 1: (from right to left) Find the Violator, which is the 1st element violating 'increase trend'; if no violator, reverse the entire array
+        // Step 2: (from right to left) Find the Changer, which is the 1st element greater than the Violator
+        // Step 3: swap Violator and Changer
+        // Step 4: reverse all elements behind Violator index
 
-        //Step 1: (from right to left), find the 1st element violating 'increase trend'. If not, rearrange in ascending order
-        //Step 2: (from right to left), find the 1st element, calling ChangeNumber which is greater than the Violator
-        //Step 3: swap elements of 1 and 2 step
-        //Step 4: reverse all elements behind Violator index
+        if (num.length < 2) return;
+        ArrayList<Integer> found = new ArrayList<Integer>();
 
-        int i = length - 1;
-        ArrayList<Integer> res = new ArrayList<Integer>();
-
-        while (i > 0) {
-            int runner = i - 1;
-            if (num[runner] < num[i]) {
-                res.add(runner);
+        for (int i = num.length - 2; i >= 0; i--) {
+            if (num[i] < num[i + 1]) {
+                found.add(i);
+                int j = num.length -1;
+                while (!(num[j] > num[i])) j--;
+                found.add(j);
                 break;
             }
-            i--;
         }
 
-        if (res.size() == 1) {
-            int j = length - 1;
-            while (j > res.get(0)) {
-                if (num[j] > num[res.get(0)]) {
-                    res.add(j);
-                    break;
-                }
-                j--;
-            }
+        if (found.isEmpty()) {
+            reverse(num, 0); // no violator found, then reverse the entire array
+        } else {
+            swap(num, found.get(0), found.get(1));
+            reverse(num, found.get(0) + 1);
         }
-
-        if (res.size() == 2) {
-            nextPermutation(num, res.get(0), res.get(1));
-            nextPermutationReverse(num, res.get(0) + 1);
-        }
-
-        if (res.size() == 0) nextPermutationReverse(num, 0);
-        return;
     }
 
-    private void nextPermutation(int[] num, int violator, int change) {
+    private void swap(int[] num, int violator, int change) {
         int temp = num[change];
         num[change] = num[violator];
         num[violator] = temp;
-        return;
     }
 
-    private void nextPermutationReverse(int[] num, int change) {
-
-        int last = num.length - 1;
-
-        while (change < last) {
-            int temp = num[change];
-            num[change] = num[last];
-            num[last] = temp;
-            change++;
-            last--;
-        }
-        return;
+    private void reverse(int[] num, int change) {
+        for (int i = num.length - 1; i > change; i--, change++) swap(num, i, change);
     }
-
 }
