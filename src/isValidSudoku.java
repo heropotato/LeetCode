0f809f21c5e713c11fb3a5c9e66c91a8b12f078f
Yong WEN <heropotato1130@msn.com>
2014-01-03 23:07:19 -0500

    Valid Sudoku

--- src/isValidSudoku.java
+++ src/isValidSudoku.java
@@ -15,9 +15,7 @@ public class isValidSudoku {
     * Valid Sudoku
     *
     * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
-    *
     * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
-    *
     * A partially filled sudoku which is valid.
     *
     * */
@@ -27,92 +25,45 @@ public class isValidSudoku {
         // DO NOT write main() function
 
         ArrayList<ArrayList<Character>> rows = new ArrayList<ArrayList<Character>>();
-        ArrayList<ArrayList<Character>> columns = new ArrayList<ArrayList<Character>>();
+        ArrayList<ArrayList<Character>> cols = new ArrayList<ArrayList<Character>>();
         ArrayList<ArrayList<Character>> sets = new ArrayList<ArrayList<Character>>();
 
-        int count = 0;
-        while (count < 9) {
-            ArrayList<Character> tempR = new ArrayList<Character>();
-            ArrayList<Character> tempC = new ArrayList<Character>();
-            ArrayList<Character> tempS = new ArrayList<Character>();
-            rows.add(tempR);
-            columns.add(tempC);
-            sets.add(tempS);
-            count++;
+        for (int i = 0; i < 9; i++) {
+            rows.add(new ArrayList<Character>());
+            cols.add(new ArrayList<Character>());
+            sets.add(new ArrayList<Character>());
         }
 
-        int row = 0;
-        while (row < 9) {
-            int col = 0;
-            while (col < 9) {
-
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
                 char c = board[row][col];
                 rows.get(row).add(c);
-                columns.get(col).add(c);
-
-                if (row < 3) {
-                    if (col < 3) {
-                        sets.get(0).add(c);
-                    }
-                    if (col >= 3 && col < 6) {
-                        sets.get(1).add(c);
-                    }
-                    if (col >= 6) {
-                        sets.get(2).add(c);
-                    }
+                cols.get(col).add(c);
+                if (row / 3 < 1) {
+                    if (col / 3 < 1) sets.get(0).add(c);
+                    else if (col / 3 < 2) sets.get(1).add(c);
+                    else sets.get(2).add(c);
+                } else if (row / 3 < 2) {
+                    if (col / 3 < 1) sets.get(3).add(c);
+                    else if (col / 3 < 2) sets.get(4).add(c);
+                    else sets.get(5).add(c);
+                } else {
+                    if (col / 3 < 1) sets.get(6).add(c);
+                    else if (col / 3 < 2) sets.get(7).add(c);
+                    else sets.get(8).add(c);
                 }
-
-                if (row >= 3 && row < 6) {
-                    if (col < 3) {
-                        sets.get(3).add(c);
-                    }
-                    if (col >= 3 && col < 6) {
-                        sets.get(4).add(c);
-                    }
-                    if (col >= 6) {
-                        sets.get(5).add(c);
-                    }
-                }
-
-                if (row >= 6) {
-                    if (col < 3) {
-                        sets.get(6).add(c);
-                    }
-                    if (col >= 3 && col < 6) {
-                        sets.get(7).add(c);
-                    }
-                    if (col >= 6) {
-                        sets.get(8).add(c);
-                    }
-                }
-                col++;
             }
-            row++;
         }
 
-        count--;
-        while (count >= 0) {
-            if (!isValidSudoku(rows.get(count))) {
-            }
-            if (!isValidSudoku(rows.get(count)) || !isValidSudoku(columns.get(count)) || !isValidSudoku(sets.get(count))) {
-                return false;
-            }
-            count--;
+        for (int i = 0; i < 9; i++) {
+            if (!isValidSudoku(rows.get(i)) || !isValidSudoku(cols.get(i)) || !isValidSudoku(sets.get(i))) return false;
         }
         return true;
     }
 
     private boolean isValidSudoku(ArrayList<Character> set) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
         HashSet<Character> hashSet = new HashSet<Character>();
-
-        for (Character s : set) {
-            if (hashSet.contains(s)) return false;
-            if (s != '.') hashSet.add(s);
-        }
-
+        for (Character s : set) if (s != '.' && !hashSet.add(s)) return false;
         return true;
     }
-
 }
