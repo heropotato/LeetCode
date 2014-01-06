c26ee13cd331da1edfa412a69e851450d594f8ef
Yong WEN <heropotato1130@msn.com>
2014-01-06 17:54:15 -0500

    Jump Game II

--- src/jump.java
+++ src/jump.java
@@ -12,32 +12,28 @@ public class jump {
 * Jump Game II
 *
 * Given an array of non-negative integers, you are initially positioned at the first index of the array.
-*
 * Each element in the array represents your maximum jump length at that position.
-*
 * Your goal is to reach the last index in the minimum number of jumps.
-*
 *
 * For example:
 * Given array A = [2,3,1,1,4]
 *
 * The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
 *
-*
-*
 * */
 
     public int jump(int[] A) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
-        int moves = 0;
-        for (int i = 1, maxMoveToPos = A[0], forwardRunner = 0; i < A.length; i++) {
-            if (i > forwardRunner) {
-                //make an real move
-                forwardRunner = maxMoveToPos;
-                moves++;
+        // Iterate the array to find out furthest steps can be made, record as maxTo
+        // Once the iterator goes beyond the cur position, make an real move
+        int res = 0;
+        for (int i = 1, maxTo = A[0], cur = 0; i < A.length; i++) {
+            if (i > cur) {
+                cur = maxTo;
+                res++;
             }
-            maxMoveToPos = Math.max(maxMoveToPos, i + A[i]);
+            maxTo = Math.max(i + A[i], maxTo);
         }
-        return moves;
+        return res;
     }
 }
